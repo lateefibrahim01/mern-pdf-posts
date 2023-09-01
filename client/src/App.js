38e8100e3e0fdab610bce0add3a6f10946e55029
Lateef Ibrahim <latbra@gmail.com>
2023-09-01 13:27:48 -0700

    solving the bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,47 @@
 // App.js
 
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PDFList from './PDFList'; // Import your PDFList component
+import UploadPDF from './UploadPDF'; // Import your UploadPDF component
+import Login from './Login'; // Import your Login component
+import { useSelector } from 'react-redux';
 
 function App() {
-  return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-md">
-        <h1 className="text-2xl font-semibold mb-4">MERN PDF Uploader</h1>
+  // Example: Use a state to manage authentication status
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-        {/* Your components go here */}
-        {/* For example: */}
-        {/* <UploadPDF /> */}
-        {/* <PDFList /> */}
+  // Access the authentication state from Redux
+  const user = useSelector((state) => state.auth.user);
+
+  return (
+    <Router>
+      <div className="bg-gray-100 min-h-screen">
+        <header className="bg-blue-500 text-white p-4">
+          <h1 className="text-2xl font-semibold">Your App Name</h1>
+        </header>
+        <div className="container mx-auto py-4">
+          <Switch>
+            {/* Route for the login page */}
+            <Route path="/login">
+              <Login setIsLoggedIn={setIsLoggedIn} />
+            </Route>
+
+            {/* Route for authenticated users */}
+            <Route path="/">
+              {user ? (
+                <div>
+                  <PDFList />
+                  <UploadPDF />
+                </div>
+              ) : (
+                <Login setIsLoggedIn={setIsLoggedIn} />
+              )}
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
